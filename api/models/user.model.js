32b32b485ad91168760a52f0cd04da67af5d1379
Raju <ashfaqraju27@gmail.com>
2024-01-31 22:46:27 +0600

    create sign up api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,18 +3,18 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required : true,
+        required: true,
         unique: true,
-    },
+      },
     email: {
         type: String,
-        required : true,
+        required: true,
         unique: true,
-    },
+      },
     password: {
         type: String,
-        required : true,
-    }
+        required: true,
+      }
 },{timestamps: true});
 
 const User = mongoose.model('User', userSchema);

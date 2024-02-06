1581eb78546325dd1a47614d3bf733bcb6448e7f
Raju <ashfaqraju27@gmail.com>
2024-02-06 18:00:50 +0600

    google OAuth functionalities works done

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APP_KEY,
+  authDomain: "mern-estate-13c91.firebaseapp.com",
+  projectId: "mern-estate-13c91",
+  storageBucket: "mern-estate-13c91.appspot.com",
+  messagingSenderId: "395205637656",
+  appId: "1:395205637656:web:7722fc2bb6b22507c6756d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
